4f1f3df47d2ad4bc2bd9b4c34d995c6d2002fc93
crealyx <sanberk5416@hotmail.com>
2021-05-03 23:59:11 +0300

    Make board array populate by clicking on boxes

--- script.js
+++ script.js
@@ -1,16 +1,24 @@
-let signs = Array.from(document.querySelectorAll('.boxes')).textContent
+let game = document.querySelector('.gameboard')
+let signs = Array.from(document.querySelectorAll('.boxes'))
 
 
 
-console.log(signs);
 
-
-
+game.addEventListener('click', ev => {
+    if(ev.target.classList.contains('boxes')){
+        ev.target.textContent = 'X';
+        gameboard.board.push('x')
+        console.log(gameboard.board);
+    }
+})
 
 let gameboard = {
     board : [],
 
 };
+
+
+
 
 function Player(name,choice) {
     this.name = name;
