a9cc49005bc78b81a343820321f516f0b239d153
crealyx <sanberk5416@hotmail.com>
2021-05-04 22:54:48 +0300

    Add functionality to put signs

--- script.js
+++ script.js
@@ -1,29 +1,42 @@
 let game = document.querySelector('.gameboard')
 let signs = Array.from(document.querySelectorAll('.boxes'))
 
+let turn = 1;
 
 
+const Players = (name,sign) => {
+    return {name, sign};
+}
 
-game.addEventListener('click', ev => {
-    if(ev.target.classList.contains('boxes')){
-        ev.target.textContent = 'X';
-        gameboard.board.push('x')
-        console.log(gameboard.board);
-    }
-})
+let player1 = Players('p1','X');
+let player2 = Players('p2','O');
 
-let gameboard = {
+
+const gameBoard = {
     board : [],
 
 };
 
+let signsIds = signs.map(sign => sign.id);
+console.log(signsIds);
+game.addEventListener('click', ev => {
+    if(ev.target.classList.contains('boxes') && signsIds.includes(ev.target.id)){
+        if(turn === 1){
+            ev.target.textContent = 'X';
+            gameBoard.board.push('X')
+            turn++
+        }
+        else{
+            ev.target.textContent = 'O';
+            gameBoard.board.push('O')
+            turn--
+        }
+    }
+})
 
 
 
-function Player(name,choice) {
-    this.name = name;
-    this.choice = choice;
 
-}
+
 
 
